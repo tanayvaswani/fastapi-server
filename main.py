1a5a930baae57e07a55104dd7e5a58fa787fbd40
tanayvaswani <vaswani.tanay9@gmail.com>
2024-07-20 13:03:39 +0530

    feat: query params

--- main.py
+++ main.py
@@ -2,10 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+user_list = [
+   "Jerry",
+   "Joey",
+   "Phil"
+]
+
+
 @app.get('/health')
 async def read_root():
     return { "status": "UP" }
 
+
 @app.get('/greet/{username}')
 async def greet(username: str):
    return { "message": f"Hello {username}!!!" }
+
+
+@app.get('/search')
+async def search_for_user(username: str):
+    for user in user_list:
+        if username in user_list:
+            return { "message": f"details for user {username}" }
+
+        else:
+            return { "message": "User Not Found" }
