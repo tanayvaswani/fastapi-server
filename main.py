8f670bf35faa42fb90d1d5887a3a9aac67d14104
tanayvaswani <vaswani.tanay9@gmail.com>
2024-07-20 12:58:52 +0530

    feat: dynamic endpoint check

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get('/health')
 async def read_root():
     return { "status": "UP" }
+
+@app.get('/greet/{username}')
+async def greet(username: str):
+   return { "message": f"Hello {username}!!!" }
